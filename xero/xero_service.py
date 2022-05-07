2a07c64a883695ef200076080dec869c005a0718
HM <hieumdd@gmail.com>
2022-05-07 23:23:44 +0700

    feat: Add get_timeframe_service

--- xero/xero_service.py
+++ xero/xero_service.py
@@ -1,22 +1,31 @@
 from typing import Union, Optional
+from datetime import datetime
 
 from compose import compose
 
 from xero.pipeline.interface import Pipeline
 from xero.repo import get_listing
-from db.bigquery import load
+from db.bigquery import get_last_timestamp, load
+
+
+def _get_timeframe_service(pipeline: Pipeline):
+    def _svc(start: Optional[str]) -> datetime:
+        return (
+            get_last_timestamp(pipeline.name, pipeline.cursor_key)
+            if not start
+            else datetime.strptime(start, "%Y-%m-%d")
+        )
+    return _svc
 
 
 def pipeline_service(
     pipeline: Pipeline,
     start: Optional[str],
-    end: Optional[str],
 ) -> dict[str, Union[str, int]]:
     return compose(
         lambda x: {
             "table": pipeline.name,
             "start": start,
-            "end": end,
             "output_rows": x,
         },
         load(
@@ -26,5 +35,6 @@ def pipeline_service(
             pipeline.cursor_key,
         ),
         pipeline.transform,
-        get_listing(pipeline.uri, pipeline.params, pipeline.res_fn)
-    )((start, end))
+        get_listing(pipeline.uri, pipeline.params, pipeline.res_fn),
+        _get_timeframe_service(pipeline),
+    )(start)
