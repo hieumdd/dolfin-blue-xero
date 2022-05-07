740abcbf702f0cf1386dbedb55357c08a937c206
HM <hieumdd@gmail.com>
2022-05-07 23:23:25 +0700

    feat: Add OAuth2 & retry mechanism

--- xero/repo.py
+++ xero/repo.py
@@ -1,17 +1,40 @@
 from typing import Any, Callable
 import os
 from datetime import datetime
+import time
 
-from authlib.integrations.requests_client import OAuth2Session
+from authlib.integrations.httpx_client import OAuth2Client
 
 BASE_URL = "https://api.xero.com/api.xro/2.0/"
 
 
 def get_client():
-    return OAuth2Session(
-        client_id=os.getenv("XERO_CLIENT_ID"),
-        client_secret=os.getenv("XERO_CLIENT_SECRET"),
-        token_endpoint="https://identity.xero.com/connect/token",
+    auth_info = {
+        "client_id": os.getenv("XERO_CLIENT_ID"),
+        "client_secret": os.getenv("XERO_CLIENT_SECRET"),
+    }
+    auth_session = OAuth2Client(
+        **auth_info,
+        scope=" ".join(
+            [
+                "accounting.contacts.read",
+                "accounting.budgets.read",
+                "accounting.reports.read",
+                "accounting.transactions.read",
+            ]
+        ),
+    )
+    token = auth_session.fetch_token(
+        "https://identity.xero.com/connect/token",
+        grant_type="client_credentials",
+    )
+    return OAuth2Client(
+        **auth_info,
+        token=token,
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
     )
 
 
@@ -21,13 +44,20 @@ def get_listing(
     res_fn: Callable[[dict[str, Any]], list[dict[str, Any]]],
 ):
     def _get(start: datetime):
-        def __get(client: OAuth2Session, page: int = 1):
-            with client.get(
-                f"{BASE_URL}/{uri}", params=params, headers={"If-Modified-Since": start}
-            ) as r:
+        def __get(client: OAuth2Client, page: int = 1):
+            r = client.get(
+                f"{BASE_URL}/{uri}",
+                params={**params, "page": page},
+                headers={"If-Modified-Since": start.isoformat(timespec="seconds")},
+            )
+            if r.status_code == 429:
+                time.sleep(2)
+                return __get(client, page)
+            else:
+                r.raise_for_status()
                 res = r.json()
-            data = res_fn(res)
-            return data if not data else __get(client, page + 1)
+                data = res_fn(res)
+                return data if not data else data + __get(client, page + 1)
 
         with get_client() as client:
             return __get(client)
