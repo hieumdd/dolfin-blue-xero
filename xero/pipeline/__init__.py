e1884dc22a911d2b9b7769028bf6f99de6684404
hungnv2311 <hungnv2311@gmail.com>
2022-05-10 16:55:43 +0700

    feat: Add Accounts

--- xero/pipeline/__init__.py
+++ xero/pipeline/__init__.py
@@ -1,10 +1,11 @@
-from xero.pipeline import bank_transactions, contacts, invoices, purchase_orders, credit_notes
+from xero.pipeline import accounts, bank_transactions, contacts, invoices, purchase_orders, credit_notes
 
 pipelines = {
     i.name: i
     for i in [
         j.pipeline
         for j in [
+            accounts,
             bank_transactions,
             contacts,
             invoices,
